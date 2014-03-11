29d2d48f985c497afe59f00c73fce2db737fa975
Ryan Ostrager <ryan.ostrager@gmail.com>
2014-03-11 16:13:43 +0000

    Fixed bug introduced in last commit.

--- app/public/Negotiation/index.js
+++ app/public/Negotiation/index.js
@@ -1,7 +1,4 @@
-$(document).ready(function() {
-  console.log('Ready page Start');
   world = '001';
   room = 'middle';
   currentRoom = room;  //Maybe declare all sessions when ready with event handlers.
                        //Then join various sessions on click
-});
