0027a3644e9950d06348ad33f69c73f0c8a381f8
Ryan Ostrager <ryan.ostrager@gmail.com>
2014-05-05 13:13:13 +0000

    Quick hack to add in support for additional worlds.

--- app/app.js
+++ app/app.js
@@ -18,27 +18,25 @@ app.configure(function() {
 });
 //world data
 var worlds = {};
-var world = {};
-worlds['001'] = world;
-world.name = 'negotiation';
-world.roomCount = 3;
-world.roomNames = [];
-world.roomNames[0] = 'union';
-world.roomNames[1] = 'management';
-world.roomNames[2] = 'middle';
-console.log('world: %j', worlds);
+worlds['red']={};
+worlds['red'].name = 'negotiation';
+worlds['red'].roomCount = 3;
+worlds['red'].roomNames = [];
+worlds['red'].roomNames[0] = 'union';
+worlds['red'].roomNames[1] = 'management';
+worlds['red'].roomNames[2] = 'middle';
 //OpenTOk Config
 var location = config.opentok.location;
 var opentokAPP = new opentok.OpenTokSDK(config.opentok.key, config.opentok.secret);
 var tokens = {};
 var sessions = {};
 var ii;
-for (ii = 0; ii < world.roomCount; ii++) {
+for (ii = 0; ii < worlds['red'].roomCount; ii++) {
   var session = {};
-  session.sessionName = world.roomNames[ii];
+  session.sessionName = worlds['red'].roomNames[ii];
   session.sessionId = 'NOT';
   session.token = '';
-  sessions[world.roomNames[ii]] = session;
+  sessions[worlds['red'].roomNames[ii]] = session;
 }
 for (var key in sessions) {
   console.log('key: ' + key);
@@ -49,6 +47,94 @@ for (var key in sessions) {
     };
   }(key));
 }
+worlds['red'].sessions=sessions;
+worlds['blue']={};
+worlds['blue'].name = 'negotiation';
+worlds['blue'].roomCount = 3;
+worlds['blue'].roomNames = [];
+worlds['blue'].roomNames[0] = 'union';
+worlds['blue'].roomNames[1] = 'management';
+worlds['blue'].roomNames[2] = 'middle';
+//OpenTOk Config
+var location = config.opentok.location;
+var bluetokens = {};
+var bluesessions = {};
+var ii;
+for (ii = 0; ii < worlds['blue'].roomCount; ii++) {
+  var session = {};
+  session.sessionName = worlds['blue'].roomNames[ii];
+  session.sessionId = 'NOT';
+  session.token = '';
+  bluesessions[worlds['blue'].roomNames[ii]] = session;
+}
+for (var key in sessions) {
+  console.log('key: ' + key);
+  opentokAPP.createSession(location, { 'p2p.preference': 'disabled' }, function(aKey) {
+    return function(result) {
+      bluesessions[aKey.toString()].sessionId = result;
+      console.log('sessions[' + aKey.toString() + '].sessionId: ', sessions[aKey].sessionId);
+    };
+  }(key));
+}
+worlds['blue'].sessions=bluesessions;
+worlds['green']={};
+worlds['green'].name = 'negotiation';
+worlds['green'].roomCount = 3;
+worlds['green'].roomNames = [];
+worlds['green'].roomNames[0] = 'union';
+worlds['green'].roomNames[1] = 'management';
+worlds['green'].roomNames[2] = 'middle';
+//OpenTOk Config
+var location = config.opentok.location;
+var greentokens = {};
+var greensessions = {};
+var ii;
+for (ii = 0; ii < worlds['green'].roomCount; ii++) {
+  var session = {};
+  session.sessionName = worlds['green'].roomNames[ii];
+  session.sessionId = 'NOT';
+  session.token = '';
+  greensessions[worlds['green'].roomNames[ii]] = session;
+}
+for (var key in sessions) {
+  console.log('key: ' + key);
+  opentokAPP.createSession(location, { 'p2p.preference': 'disabled' }, function(aKey) {
+    return function(result) {
+      greensessions[aKey.toString()].sessionId = result;
+      console.log('sessions[' + aKey.toString() + '].sessionId: ', sessions[aKey].sessionId);
+    };
+  }(key));
+}
+worlds['green'].sessions=greensessions;
+worlds['yellow']={};
+worlds['yellow'].name = 'negotiation';
+worlds['yellow'].roomCount = 3;
+worlds['yellow'].roomNames = [];
+worlds['yellow'].roomNames[0] = 'union';
+worlds['yellow'].roomNames[1] = 'management';
+worlds['yellow'].roomNames[2] = 'middle';
+//OpenTOk Config
+var location = config.opentok.location;
+var yellowtokens = {};
+var yellowsessions = {};
+var ii;
+for (ii = 0; ii < worlds['yellow'].roomCount; ii++) {
+  var session = {};
+  session.sessionName = worlds['yellow'].roomNames[ii];
+  session.sessionId = 'NOT';
+  session.token = '';
+  yellowsessions[worlds['yellow'].roomNames[ii]] = session;
+}
+for (var key in sessions) {
+  console.log('key: ' + key);
+  opentokAPP.createSession(location, { 'p2p.preference': 'disabled' }, function(aKey) {
+    return function(result) {
+      yellowsessions[aKey.toString()].sessionId = result;
+      console.log('sessions[' + aKey.toString() + '].sessionId: ', sessions[aKey].sessionId);
+    };
+  }(key));
+}
+worlds['yellow'].sessions=yellowsessions;
 app.configure('development', function() {
   app.use(express.errorHandler({
     dumpExceptions: true,
@@ -74,6 +160,7 @@ app.get('/api/:world/:user/:room', function(req, res) {
   data.user = req.params.user;
   data.sessionName = req.params.room;
   data.apikey = config.opentok.key;
+  var sessions = worlds[req.params.world].sessions;
   //data.sessionId = sessions[session.sessionName].sessionId;
   //loop sessions
   for (var key in sessions) {
